b296e569b510b05a4fad2a9f8760cc12a8bc376a
GuilleMualem <guillermo.mualem@gmail.com>
2024-03-01 13:44:59 +0100

    upload code

---
+++ SimpleStorage.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; // solidity version
+contract SimpleStorage {
+
+uint256 myfavoriteNumber;
+
+
+struct Person{
+    uint256 favoriteNumber;
+    string name;
+}
+
+Person[] public listOfPeople;
+
+
+mapping(string => uint256) public nameToFavoriteNumber;
+
+
+function store(uint256 _favoriteNumber) public {
+    myfavoriteNumber = _favoriteNumber;
+  
+}
+
+function retrieve() public view returns(uint256){
+    return myfavoriteNumber;
+}
+
+function addPerson(string memory _name, uint256 _favoriteNumber) public {
+    
+    listOfPeople.push( Person(_favoriteNumber, _name) );
+    nameToFavoriteNumber[_name] = _favoriteNumber;
+}
+}
